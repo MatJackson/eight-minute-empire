7f1dd89e4d049de7deb1a7855559bd1b71982c23
Tamar Merdkhanian <tamar.merdkhanian@gmail.com>
2019-11-10 11:14:02 -0500

    add more print statements to driver

--- Maps/MapSingletonDriver.cpp
+++ Maps/MapSingletonDriver.cpp
@@ -10,16 +10,27 @@ using namespace std;
 
 int main() {
 
-    Map* map = Map::getInstance();
+    Map* map1 = Map::getInstance();
     Map* map2 = Map::getInstance();
 
     cout << "Are both maps the same? ";
 
     string answer = "no!";
-    if (map == map2) {
+    if (map1 == map2) {
         answer = "Yes :)";
     }
 
     cout << answer;
+
+    Continent* northAmerica = new Continent("North America");
+    Country* canada = new Country("Canada", northAmerica);
+
+    map1->addCountry(canada);
+
+    cout << "\n\nMap 1: ";
+    map1->display();
+
+    cout << "\nMap 2: ";
+    map2->display();
 
 }
